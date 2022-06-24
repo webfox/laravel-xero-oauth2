16a4381dc4f728217410453e9d693082997747de
agallop@celestra.co.uk <agallop@celestra.co.uk>
2022-06-24 17:44:35 +0100

    Multi-Tenant-Storage

--- src/Oauth2CredentialManagers/FileStore.php
+++ src/Oauth2CredentialManagers/FileStore.php
@@ -42,9 +42,18 @@ class FileStore implements OauthCredentialManager
         return $this->data('refresh_token');
     }
 
-    public function getTenantId(): string
+    public function getTenants(): ?array
     {
-        return $this->data('tenant_id');
+        return $this->data('tenants');
+    } 
+
+    public function getTenantId(int $tenant =0): string
+    {
+        if(!isset($this->data('tenants')[$tenant]))
+        {
+            throw new \Exception("No such tenant exists");
+        }
+        return $this->data('tenants')[$tenant]['Id'];
     }
 
     public function getExpires(): int
@@ -90,14 +99,14 @@ class FileStore implements OauthCredentialManager
         $this->store($newAccessToken);
     }
 
-    public function store(AccessTokenInterface $token, string $tenantId = null): void
+    public function store(AccessTokenInterface $token, array $tenants = null): void
     {
         $ret = $this->disk->put($this->filePath, json_encode([
             'token'         => $token->getToken(),
             'refresh_token' => $token->getRefreshToken(),
             'id_token'      => $token->getValues()['id_token'],
             'expires'       => $token->getExpires(),
-            'tenant_id'     => $tenantId ?? $this->getTenantId()
+            'tenants'       => $tenants ?? $this->getTenants()
         ]), 'private');
 
         if ($ret === false) {
