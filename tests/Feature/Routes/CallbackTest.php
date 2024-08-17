b6d016a328688824599bf0eb18979cc1d9631f85
James Freeman <me@james-freeman.info>
2024-08-17 23:45:52 +0100

    adding more tests

--- tests/Feature/Routes/CallbackTest.php
+++ tests/Feature/Routes/CallbackTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Webfox\Xero\Feature\Routes;
 
+use Exception;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Support\Facades\Event;
@@ -12,6 +13,7 @@ use Tests\Webfox\Xero\TestCase;
 use Tests\Webfox\Xero\TestSupport\MockAccessToken;
 use Webfox\Xero\Clients\IdentityAPIClient;
 use Webfox\Xero\Events\XeroAuthorized;
+use Webfox\Xero\Exceptions\OAuthException;
 use Webfox\Xero\Oauth2Provider;
 use Webfox\Xero\XeroOAuth;
 
@@ -93,6 +95,59 @@ class CallbackTest extends TestCase
             ]))
             ->assertRedirect($from)
             ->assertSessionHasErrors('state');
+
+        Event::assertNotDispatched(XeroAuthorized::class);
+    }
+
+    public function test_if_there_is_an_error_an_oauth_exception_will_be_thrown()
+    {
+        Event::fake();
+        XeroOAuth::fake();
+
+        Session::put('xero_oauth2_state', 'foo');
+
+        $this->mock(Oauth2Provider::class, function (MockInterface $mock) {
+            $mock->shouldReceive('getAccessToken')
+                ->andReturn(new MockAccessToken);
+        });
+
+        $this->withoutExceptionHandling();
+
+        $this->expectException(OAuthException::class);
+        $this->expectExceptionMessage('Error: Error Description');
+
+        $this->get(route('xero.auth.callback', [
+                'state' => 'foo',
+                'code' => 'bar',
+                'error' => 'error',
+                'error_description' => 'error description',
+            ]));
+
+        Event::assertNotDispatched(XeroAuthorized::class);
+    }
+
+    public function test_that_it_will_handle_on_failure()
+    {
+        Event::fake();
+        XeroOAuth::fake();
+
+        Session::put('xero_oauth2_state', 'foo');
+
+        $this->mock(Oauth2Provider::class, function (MockInterface $mock) {
+            $mock->shouldReceive('getAccessToken')
+                ->andThrow(new Exception('foo'));
+        });
+
+        $this->withoutExceptionHandling();
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('foo');
+
+        $this->get(route('xero.auth.callback', [
+                'state' => 'foo',
+                'code' => 'bar',
+            ]))
+            ->assertInternalServerError();
 
         Event::assertNotDispatched(XeroAuthorized::class);
     }
