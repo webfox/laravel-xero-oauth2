a61ab5815b99e13be9b5865e5f9d4fe61ba9300c
James Freeman <me@james-freeman.info>
2024-08-23 21:26:34 +0100

    Remove unneeded setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Webfox\Xero;
 
-use Illuminate\Support\Facades\File;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Webfox\Xero\XeroServiceProvider;
 
@@ -11,29 +10,6 @@ class TestCase extends Orchestra
     protected function getEnvironmentSetUp($app): void
     {
         config()->set('app.key', 'base64:dW5venZpb3V2eDRkbHJjaHV3dDR5aW9mcnFpNzFrOTA=');
-
-        $this->initializeDirectory($this->getTempDirectory());
-
-        config()->set('filesystems.disks.public', [
-            'driver' => 'local',
-            'root' => $this->getTempDirectory(),
-            'url' => '/tmp',
-        ]);
-
-        $app->bind('path.public', fn () => $this->getTempDirectory());
-    }
-
-    protected function initializeDirectory($directory): void
-    {
-        if (File::isDirectory($directory)) {
-            File::deleteDirectory($directory);
-        }
-        File::makeDirectory($directory);
-    }
-
-    public function getTempDirectory(string $suffix = ''): string
-    {
-        return __DIR__.'/TestSupport/temp'.($suffix == '' ? '' : '/'.$suffix);
     }
 
     /**
